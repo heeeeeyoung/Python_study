c9ae201d56bf676442585827ee0355878801d7db
kimheeyoung <khy11009@naver.com>
2023-09-13 23:17:37 +0900

    day 2

---
+++ python02/2-2.py
@@ -0,0 +1,142 @@
+# food="Python's favorite food is perl"
+
+# food
+# "Python's favorite food is perl"
+
+
+# food='Python's favorite food is perl'
+# SyntaxError: unterminated string literal (detected at line 1)
+
+
+# say
+# Traceback (most recent call last):
+#   File "<pyshell#9>", line 1, in <module>
+#     say
+# NameError: name 'say' is not defined
+
+
+# say='"Python is very easy." he says.'
+
+# say
+# '"Python is very easy." he says.'
+
+
+
+# food = 'Python\'s favorite food is perl'
+# say = "\"Python is very easy.\" he says."
+
+# print(food)
+# Python's favorite food is perl
+# print(say)
+# "Python is very easy." he says.
+
+
+# multiline="Life is too short.\n You need Python."
+
+# print(multiline)
+# Life is too short.
+# You need Python.
+
+
+
+# multiline='''
+# Life is too short.
+# You need Python.
+# '''
+
+# print(multiline)
+
+# Life is too short.
+# You need Python.
+
+
+
+# multiline="""
+# Life is too short.
+# You need Python.
+# """
+
+# print(multiline)
+
+# Life is too short.
+# You need Python.
+
+
+
+
+# head = "Python"
+
+# tail = "is fun."
+
+# head + tail
+# 'Pythonis fun.'
+
+
+
+# a="Python"
+# a*2
+# 'PythonPython'
+
+
+
+print("="*50)
+print("My Program")
+print("="*50)
+
+
+# a="Life is too short."
+# len(a)
+# 18
+
+
+# b="You need Python."
+# len(b)
+# 16
+
+
+# c="Life is too short, You need Python."
+# c[3]
+# 'e'
+
+
+# d="Life is too short, You need Python."
+# d[0]
+# 'L'
+
+# d[12]
+# 's'
+
+# d[-1]
+# '.'
+
+# d[-0]
+# 'L'
+
+# d[-2]
+# 'n'
+
+# d[-5]
+# 't'
+
+
+
+# a="Life is too short, You need Python"
+# b=a[0]+a[1]+a[2]+a[3]
+# b
+# 'Life'
+
+
+# a[0:4]
+# 'Life'
+
+
+
+# a[0:3]
+# 'Lif'
+
+
+# a[0:5]
+# 'Life '
+
+
+
