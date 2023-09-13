f8d804afcfc8e6c7bac049ac4d7cca51faa7008b
kimheeyoung <khy11009@naver.com>
2023-09-13 23:17:06 +0900

    day 1

---
+++ python01/1-1.py
@@ -0,0 +1,70 @@
+# Python 3.11.5 (v3.11.5:cce6ba91b3, Aug 24 2023, 10:50:31) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+# Type "help", "copyright", "credits" or "license()" for more information.
+
+# 1+1
+# 2
+
+
+# 1+2
+# 3
+
+
+# 3/2.4
+# 1.25
+
+
+# 3*9
+# 27
+
+
+# a=1
+# b=2
+# a+b
+# 3
+
+
+# a="Python"
+# print(a)
+# Python
+
+
+# a
+# 'Python'
+
+
+# a=3
+# if a>1:
+#           print("a is greater than 1")
+
+# a is greater than 1
+
+
+
+# for a in [1, 2, 3]:
+#           print(a)
+
+
+# 1
+# 2
+# 3
+
+
+
+# i=0
+
+# while i<3:
+#     i=i+1
+#     print(i)
+
+
+# 1
+# 2
+# 3
+
+
+
+# def add(a, b):
+#     return a+b
+
+# add(3, 4)
+# 7
